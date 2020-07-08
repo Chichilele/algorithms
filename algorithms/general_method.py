1f695dbbe7f5dad73616712e4d29dd8df737e78e
G <gauthier.castro@ecs.co.uk>
2020-07-09 00:26:17 +0100

    add general_method.py

---
+++ algorithms/general_method.py
@@ -0,0 +1,22 @@
+class Method:
+    def __init__(self,):
+
+        """ Generic method class for calculating and 
+		visualizing a root-finding algorithm.
+	
+		Attributes:
+			"""
+
+    def example(self,):
+
+        """Description. Input description. Function description.
+				
+		Args:
+			arg1 (type): description
+		
+		Returns:
+			type: description
+		
+		"""
+
+        pass
