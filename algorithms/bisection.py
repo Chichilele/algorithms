6739f5d8de815ac8718e9b8d9206aa3581989b56
G <gauthier.castro@ecs.co.uk>
2020-07-09 04:08:12 +0100

    add plot_epochs()

--- algorithms/bisection.py
+++ algorithms/bisection.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import sign
 
 from .general_method import Method
 
@@ -13,31 +13,53 @@ class Bisection(Method,):
     def __init__(self, f=None):
         Method.__init__(self)
         self.f = f
+        self.epochs = None
 
     def solve(self, a, b, tol=10e-3, max_iter=100):
 
-        """
+        """run the Bisection root-finding method starting between points a and b. 
+        Stops if tolerance is matched or raise error if max interation reached.
+
+        Args:
+            a (float): first interval boundary
+            b (float): second interval boundary
+            tol (float): tolerance error on objective (c)
+            max_iter (int): maximum number of iterations before interrupting the algorithm.
+
+        Returns:
+            float: compute root within interval.
         """
 
-        if self.f(a) == 0:
+        self.epochs = []
+        if 0 == self.f(a):
             return a
-        elif self.f(b) == 0:
+        elif 0 == self.f(b):
             return b
 
-        if np.sign(self.f(a)) == np.sign(self.f(b)):
+        if sign(self.f(a)) == sign(self.f(b)):
             raise ValueError(
-                f"Same sign. f(a): {np.sign(self.f(a))},\tf(b): {np.sign(self.f(b))}"
+                f"Same sign. f(a): {sign(self.f(a))},\tf(b): {sign(self.f(b))}"
             )
 
         for n in range(max_iter):
             c = (a + b) / 2
+            self.epochs.append(c)
             if (0 == self.f(c)) or ((b - a) / 2 < tol):  # solution found
                 return c
 
-            if np.sign(self.f(c)) == np.sign(self.f(a)):  # new interval
+            if sign(self.f(c)) == sign(self.f(a)):  # new interval
                 a = c
             else:
                 b = c
         
         raise Exception("Reached maximum number of iterations without finding a solution")
 
+    def plot_epochs(self):
+
+        """ plot the epochs from solve.
+        """
+
+        import matplotlib.pyplot as plt
+        plt.scatter(self.epochs, self.f(self.epochs), c=range(len(self.epochs)))
+        plt.ylabel('Epochs')
+        plt.show()
