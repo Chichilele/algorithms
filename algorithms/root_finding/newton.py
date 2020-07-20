ce3192c6d8e88dc7695bd248bd811c1af320aeb2
G <gauthier.castro@ecs.co.uk>
2020-07-20 10:29:42 +0100

    add class Newton

--- algorithms/root_finding/newton.py
+++ algorithms/root_finding/newton.py
@@ -4,8 +4,39 @@ from .general_method import Method
 
 
 class Newton(Method):
-    pass
+    """Newton class for Newton-Raphson root-finding method.
+    """
 
+    def __init__(self, f, fprim, x0, tol=1e-2, max_iter=1000):
+        Method.__init__(self, f=f, tol=tol, max_iter=max_iter)
 
-if __name__ == "__main__":
-    print("Algorithms")
+        self.fprim = fprim
+        self.x0 = x0
+
+    def solve(self):
+        """Solve Newton root-finding method
+        """
+
+        ## init
+        self.epochs = [self.x0]
+
+        ## lucky initial guess
+        if 0 == self.f(self.epochs[-1]):
+            return self.epochs[-1]
+
+        ## loop until max iter
+        for _ in range(self.max_iter):
+            self.epochs.append(
+                self.epochs[-1] - self.f(self.epochs[-1]) / self.fprim(self.epochs[-1])
+            )
+
+            convergence = np.abs(self.f(self.epochs[-2]) - self.f(self.epochs[-1]))
+            if self.tol >= convergence:
+                return self.epochs[-1]
+
+        raise Exception(
+            "Convergence error. Reached maximum number of iterations without finding a stable enough solution"
+        )
+
+    def __repr__(self):
+        return super().__repr__()
--- algorithms/root_finding/tests/newtown_test.py
+++ algorithms/root_finding/tests/newtown_test.py
@@ -6,16 +6,33 @@
 
 import unittest
 
-from algorithms import Newton
+from algorithms.root_finding import Newton
 
 
 class TestNewtonClass(unittest.TestCase):
     def setUp(self):
-        self.newton = Newton()
+        f = lambda x: x ** 2
+        fprim = lambda x: 2 * x
+        x0 = -1
 
-    # def test_initialization(self):
-    #     self.assertEqual(self.newton.p, 0.4, "p value incorrect")
-    #     self.assertEqual(self.newton.n, 20, "n value incorrect")
+        self.newton = Newton(f, fprim, x0)
+
+    def test_initialization(self):
+        self.assertEqual(self.newton.f(2), 4, "incorrect f init")
+        self.assertEqual(self.newton.fprim(3), 6, "incorrect fprim init")
+        self.assertEqual(self.newton.tol, 1e-2, "incorrect tol init")
+        self.assertEqual(self.newton.max_iter, 1000, "incorrect max_iter init")
+        self.assertEqual(
+            self.newton.__repr__(),
+            "Newton: tolerance(0.01)\tmax iter(1000)",
+            "incorrect repr init",
+        )
+
+    def test_solve(self):
+        self.assertLessEqual(self.newton.solve(), 10 - 1)
+        self.newton.max_iter = 1
+        with self.assertRaises(Exception):
+            self.newton.solve()
 
 
 if __name__ == "__main__":
