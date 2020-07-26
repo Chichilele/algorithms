88064435a53bcde4fe580c7efedd2cc74e055a06
G <gauthier.castro@ecs.co.uk>
2020-07-26 18:38:04 +0100

    Add Secant class tests

--- algorithms/root_finding/tests/secant_test.py
+++ algorithms/root_finding/tests/secant_test.py
@@ -1,21 +1,33 @@
-# Any changes to the distributions library should be reinstalled with
-#  pip install --upgrade .
-
-# For running unit tests, use
-# /usr/bin/python -m unittest test
-
 import unittest
 
-from algorithms import Secant
+from algorithms.root_finding import Secant
 
 
 class TestSecantClass(unittest.TestCase):
     def setUp(self):
-        self.secant = Secant()
+        f = lambda x: x ** 3 - x + 13
+        x0 = -1
+        x1 = 10
 
-    # def test_initialization(self):
-    #     self.assertEqual(self.secant.p, 0.4, "p value incorrect")
-    #     self.assertEqual(self.secant.n, 20, "n value incorrect")
+        self.secant = Secant(f, x0, x1)
+
+    def test_initialization(self):
+        self.assertEqual(self.secant.f(-3), -11, "incorrect f init")
+        self.assertEqual(self.secant.x0, -1, "incorrect x0 init")
+        self.assertEqual(self.secant.x1, 10, "incorrect x0 init")
+        self.assertEqual(self.secant.tol, 1e-2, "incorrect tol init")
+        self.assertEqual(self.secant.max_iter, 1000, "incorrect max_iter init")
+        self.assertEqual(
+            self.secant.__repr__(),
+            "Secant: tolerance(0.01)\tmax iter(1000)",
+            "incorrect repr init",
+        )
+
+    def test_solve(self):
+        self.assertLessEqual(self.secant.solve(), 10e-1)
+        self.secant.max_iter = 1
+        with self.assertRaises(Exception):
+            self.secant.solve()
 
 
 if __name__ == "__main__":
