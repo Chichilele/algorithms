2851f2b5fa240b7c5e75d835d6eb35a531a95d0d
G <gauthier.castro@ecs.co.uk>
2020-07-27 10:14:07 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="algorithms",
     version="0.1",
     description="Implements a few optimisation algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chichilele/algorithms",
     packages=find_packages(),
-    install_requires=["numpy"],
-    entry_points={"console_scripts": ["algorithms=algorithms.newton:cli"]},
+    entry_points={"console_scripts": ["root_finding=algorithms.root_finding:cli"]},
 )
